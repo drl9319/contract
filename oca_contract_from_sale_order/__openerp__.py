5912705d08e523cfe8306d45d7144f6c9b78e613
Daniel Rodriguez <drodriguez@praxya.com>
2016-06-29 09:10:24 +0200

    WIP

--- oca_contract_from_sale_order/__openerp__.py
+++ oca_contract_from_sale_order/__openerp__.py
@@ -4,11 +4,11 @@
 
     'summary': """stock""",
 
-    'author': "Praxya",
-    'website': "http://praxya.com",
+    'author': "Daniel Rodriguez, Odoo Community Association (OCA)",
+    'website': "http://danielrodriguez.esy.es/blog/",
     # Category se usa com filtrro para la lista de modulos
     'category': 'Other',
-    'version': '8.0.1',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     # Depends indica los modulos necesarios para el correcto trabajo de nuestro modulo
     'depends': [
